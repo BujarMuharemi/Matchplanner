c6404489b6d9b68e36595fb98b263f8f89d45103
Kocak <Kocak@10.93.23.67>
2018-12-11 17:01:28 +0100

    removed classpath & project

--- src/FileIO/OpenXLS.java
+++ src/FileIO/OpenXLS.java
@@ -1,5 +1,29 @@
 package FileIO;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+import model.Spiel;
+
 public class OpenXLS {
 
+	String path;
+	List<Spiel> daten;
+
+	public OpenXLS(String openpath) {
+		this.path = openpath;
+		
+		try {
+			//TODO:Daten auslesen aus den XLS datei!
+			FileInputStream fis = new FileInputStream(path);
+			XSSLWorkbook wb 
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 }
