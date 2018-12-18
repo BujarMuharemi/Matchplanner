b74e6137d85cfcf1f13e1995d96bdbac48f9de10
Kocak <Kocak@10.93.23.67>
2018-12-18 14:00:49 +0100

    SavePDF eingebunden!

--- src/FileIO/SavePDF.java
+++ src/FileIO/SavePDF.java
@@ -1,9 +1,20 @@
 package FileIO;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.text.Document;
+
 import model.Spiel;
 import model.TabelleSpielTage;
+import com.itextpdf.*;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
 
 public class SavePDF {
 
@@ -11,11 +22,25 @@ public class SavePDF {
 	List<Spiel> daten = new ArrayList<>();
 	public SavePDF(String path, TabelleSpielTage tabelle) {
 		this.path = path;
-
+		this.daten =tabelle.getSpielTageOutput();
 		writePDF(tabelle);
 	}
 
 	public void writePDF(TabelleSpielTage tabelle) {
-	    
+		try {
+			PdfWriter writer = new PdfWriter(new File(path));
+			PdfDocument pdfDoc = new PdfDocument(writer);
+			com.itextpdf.layout.Document document = new com.itextpdf.layout.Document(pdfDoc);
+			Table tabel = new Table(daten.size());
+			for (Spiel spiel : daten) {
+				tabel.addCell(spiel.getM1().getName());
+				
+			}
+			document.add(tabel);
+			document.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
